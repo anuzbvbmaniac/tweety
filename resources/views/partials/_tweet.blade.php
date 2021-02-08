af05bcdfcad6258a9ddf9b28f9ad4662d57adf1d
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2021-02-09 02:26:36 +0545

    Changes: Main View Created. 1. Main View with:    a. Sidebar Links    b. Tweet Panel    c. Tweet    d. Friends List.

---
+++ resources/views/partials/_tweet.blade.php
@@ -0,0 +1,12 @@
+<div class="flex p-4 border-b border-b-grey-500">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consectetur nihil quae. Ad blanditiis, doloremque ex inventore magni, minima mollitia necessitatibus obcaecati quaerat quisquam recusandae reiciendis rem veniam. Aspernatur aut commodi dicta eaque eligendi ex excepturi harum incidunt ipsa labore nostrum quas, quos repellendus sint sit. Fugit minima possimus ullam.
+        </p>
+    </div>
+</div>
